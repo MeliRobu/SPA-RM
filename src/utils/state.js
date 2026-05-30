bdc6db3356808611f4691af50daf6de80226f92d
Melissa <melisofirb@gmail.com>
2026-05-29 20:01:12 -0500

    state funtions done and main done

--- src/utils/state.js
+++ src/utils/state.js
@@ -63,7 +63,7 @@ export const editCharacter = (id, changes) => {
             // we return it without changes
         }
 })
-    saveEditedCharacters(editedCharacters);
+    saveEditedCharacters(characters);
 }
 
 export const loadInicialData = async () => {
